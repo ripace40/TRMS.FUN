f3a8872215d9c98e617379d1923bd8b3efcadab1
ripace40 <Asawright30@gmail.com>
2026-01-08 19:05:20 -0700

    scrip.js
    Signed-off-by: ripace40 <Asawright30@gmail.com>

---
+++ scrip.js
@@ -0,0 +1,4 @@
+function searchOnGoogle(){
+    var searchQuerry = document.getElementById('searchInput').value;
+    var searchURL ='https://www.google.co.uk/search?q=' + searchQuerry;
+    window.open(searchURL, '_self');
